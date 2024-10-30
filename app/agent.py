6882f254d8ab065ae2b3dc6a496b44d148ef6d28
Stinger18 <ethanhowlett01@gmail.com>
2024-10-30 17:46:49 -0600

    comment

--- app/agent.py
+++ app/agent.py
@@ -5,6 +5,7 @@ import requests
 API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-3.2-11B-Vision-Instruct"
 headers = {"Authorization": "Bearer hf_***"}
 
+# In myKey.py define a function return_key() that returns the API key
 client = InferenceClient(api_key=return_key())
 
 image_url = "https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg"
