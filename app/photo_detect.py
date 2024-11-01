d557ff149a33bda727430963c3910db7b7f1f3e5
jamesknowles <jamesk411@gmail.com>
2024-11-01 13:43:58 -0600

    Cleaned up old files

---
+++ app/photo_detect.py
@@ -0,0 +1,8 @@
+'''
+What I think would be great for this file is to just have a class (or just function?)
+that takes in an image and spits out every ingredient in a list it thinks are in the image. 
+(Maybe the function also takes in a confidence interval?)
+'''
+
+
+
