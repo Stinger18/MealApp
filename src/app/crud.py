e2fa0e4c69381305385b3062e8b475c5dc6df4dd
Stinger18 <ethanhowlett01@gmail.com>
2024-11-13 18:30:16 -0700

    This is the correct crud.py

--- src/app/crud.py
+++ src/app/crud.py
@@ -14,9 +14,13 @@ from passlib.context import CryptContext
 # Password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def get_user(db: Session, userId: int) -> (models.User | None):
+def get_user_by_id(db: Session, userId: int) -> (models.User | None):
     ''' Returns the user (User) with the given ID'''
     return db.query(models.User).filter(models.User.id == userId).first()
+
+def get_user_by_name(db: Session, userName: str) -> (models.User | None):
+    ''' Returns the user (User) with the given name'''
+    return db.query(models.User).filter(models.User.name == userName).first()
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
@@ -33,7 +37,7 @@ def create_user(db: Session, id: int, name: str, email:str, password: str, recip
 def get_recipe(db: Session, userRecipeId: int, recipeId: int) -> (models.Recipe | None):
     return db.query(models.Recipe).filter(models.Recipe.id == recipeId, models.Recipe.ownerId == userRecipeId).first()
 
-def get_all_recipes(db: Session, userRecipeId: int):
+def get_all_recipes(db: Session, userRecipeId: int) -> list(models.Recipe):
     return db.query(models.Recipe).filter(models.Recipe.ownerId == userRecipeId).all()
 
 def create_recipe(db: Session, id:int, ownerId: int, title: str, ingredients: str, instructions: str) -> (models.Recipe | None):
