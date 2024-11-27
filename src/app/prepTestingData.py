62df1e2ea7e8fe0902f8182ad4c60034d500db46
Ethan Howlett <ethanhowlett01@gmail.com>
2024-11-27 10:24:08 -0700

    Removed and edited files form testing

--- src/app/prepTestingData.py
+++ src/app/prepTestingData.py
@@ -3,7 +3,7 @@ Use this file to create test data in the database
 '''
 from sqlalchemy.orm import Session
 from database import SessionLocal
-import models, crud, database, schemas, modelsDev
+import models, crud, database, schemas
 
 def create_test_recipe_new_model(db: Session, id: int, ownerId: int, name: str, ingredients: dict, instructions: str, servings: int, prepTime: str, cookTime: str):
     ''' Mock CRUD operation to create a recipe 
@@ -11,23 +11,23 @@ def create_test_recipe_new_model(db: Session, id: int, ownerId: int, name: str, 
     `ingredients` is a dictionary with the ingredient name as the key and the quantity as the value. \n
     `{'Chicken': '2 lbs', 'Cream': '2 cup', 'Spinach': '1 cup'}` '''
     # Create a new recipe
-    newRecipe = modelsDev.Recipe(id=id, ownerId=ownerId, name=name, instructions=instructions, servings=servings, prepTime=prepTime, cookTime=cookTime)
+    newRecipe = models.Recipe(id=id, ownerId=ownerId, name=name, instructions=instructions, servings=servings, prepTime=prepTime, cookTime=cookTime)
 
     # Disctionary of ingredients and their quantities
     ingredients = ingredients
 
     for ingredient, quantity in ingredients.items(): # For each ingredient in the dictionary
         # Check if the ingredient already exists in the database
-        exsisting_ingredient = db.query(modelsDev.Ingredient).filter(modelsDev.Ingredient.name == ingredient).first()
+        exsisting_ingredient = db.query(models.Ingredient).filter(models.Ingredient.name == ingredient).first()
 
         # If the ingredient does not exist, create a new ingredient
         if not exsisting_ingredient:
-            ingredient = modelsDev.Ingredient(name=ingredient)
+            ingredient = models.Ingredient(name=ingredient)
         else: # If the ingredient exists, use the exsisting ingredient
             ingredient = exsisting_ingredient
         
         # Associate ingredients with the recipe through the RecipeIngredient table
-        modelsDev.RecipeIngredient(recipe=newRecipe, ingredient=ingredient, quantity=quantity)
+        models.RecipeIngredient(recipe=newRecipe, ingredient=ingredient, quantity=quantity)
     # Add to the session and commit
     db.add(newRecipe)
     db.commit()
